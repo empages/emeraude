badffac3814a744e8e7e4667217cfd88c94c611f
gsk567 <georgi.karagogov@gmail.com>
2021-06-06 13:33:19 +0300

    Update SEO plugin version

--- src/Client/Definux.Emeraude.Client.EmPages/EmPagesConstants.cs
+++ src/Client/Definux.Emeraude.Client.EmPages/EmPagesConstants.cs
@@ -14,5 +14,20 @@
         /// Default page meta description key.
         /// </summary>
         public const string PageMetaTagDescriptionKey = "CurrentEmPageMetaDescription";
+
+        /// <summary>
+        /// Default page meta keywords key.
+        /// </summary>
+        public const string PageMetaTagKeywordsKey = "CurrentEmPageMetaKeywords";
+
+        /// <summary>
+        /// Default page meta image key.
+        /// </summary>
+        public const string PageMetaTagImageKey = "CurrentEmPageMetaImage";
+
+        /// <summary>
+        /// Default page meta author key.
+        /// </summary>
+        public const string PageMetaTagAuthorKey = "CurrentEmPageMetaAuthor";
     }
 }
