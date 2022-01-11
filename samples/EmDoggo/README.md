6a7f166ca5f5519f342380d9d67c37e1a9cbc24a
Georgi Karagogov <georgi.karagogov@gmail.com>
2022-01-11 02:22:22 +0200

    Remove samples from main repo

--- samples/EmDoggo/README.md
+++
@@ -1,2 +0,0 @@
-# template
-Default template for project based on Emeraude Framework
--- samples/EmDoggo/src/EmDoggo.Admin/EmPages/User/UserEmPageDataStrategy.cs
+++
@@ -1,7 +0,0 @@
-using Emeraude.Application.Admin.EmPages.Data;
-
-namespace EmDoggo.Admin.EmPages.User;
-
-public class UserEmPageDataStrategy : EmPageEntityDataStrategy<Emeraude.Infrastructure.Identity.Entities.User, UserEmPageModel>
-{
-}
